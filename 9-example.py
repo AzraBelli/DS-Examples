8ab1b66653ad351c6533aebe5108d09ca97e7076
Azra BELLI <77413791+AzraBelli@users.noreply.github.com>
2026-04-25 22:26:48 -0500

    first commit

---
+++ 9-example.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+age_list = [10,20,30,30,30,40,50,60,70,75]
+weight_list = [20,60,80,85,86,87,70,90,95,90]
+plt.plot(age_list, weight_list, 'y')
+plt.xlabel('Age')
+plt.ylabel('Weight')    
+plt.show()
+
